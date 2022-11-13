e432754e6d821e6e60849191bf5d6911fdf48c15
Islomjon1213 <islomjonkhonaliev1211@gmail.com>
2022-11-13 22:03:16 +0900

    New one

--- CSS/web2/learn.js
+++ CSS/web2/learn.js
@@ -1,18 +1,20 @@
-var first = document.getElementById('firstNumber');
-var second =  document.getElementById('secondNumber');
+var firstNum = document.getElementById('fNumber');
+var secondNum = document.getElementById('sNumber');
 var resultone = document.getElementById('result');
-var buttton = document.getElementById('buttonClick');
+var form = document.getElementById('forms');
 
-function addNumbers(blahh){
-    if(!first.value || !second.value){
-        alert("Please enter any number first");
-    } 
-    else{
-        var x = parseFloat(first.value);
-        var y = parseFloat(second.value);
-        var sum = x / y;
-        var result = sum * 100;
-        resultone.innerText = "Answer is " + result + " %";
-        blahh.preventDefault();
+
+form.addEventListener('submit', function(event){
+    if(!firstNum.value || !secondNum.value){
+        alert("Please enter any number");
+    } else{
+        var x = parseFloat(firstNum.value);
+        var y = parseFloat(secondNum.value);
+
+        var sum = x * y;
+        // var sum = x - y;
+
+        resultone.innerText = "Answer is " + sum;
+        event.preventDefault();
     }
-}
+});
